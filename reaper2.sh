f31a674dd61a15989f7cb5506b348008efb80034
OZON-SIBERIA <medvedev152@live.com>
2019-12-15 06:42:36 +1000

    remove notebook

--- reaper2.sh
+++ reaper2.sh
@@ -3,4 +3,3 @@ for loco in 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
 do
 mysql -N -e"select timestamp, rpm_diesel, power_generator, poz_kont_sec from full_sql_locodataseconds where loco_id = '$loco' and poz_kont_sec != '0'" > ./lokodata_$loco.txt -h 'lokosampledb.cozlbgcgmptq.us-east-2.rds.amazonaws.com' -u readslave -przd_2020 dbloko
 done
-
